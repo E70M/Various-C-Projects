8eb911d8b635ce2d10f2d2c53051dd880daca6aa
E70M <eitanmyron@outlook.com>
2017-06-29 13:41:58 -0700

    Work in progress

--- Dijkstra/dijkstra.cpp
+++ Dijkstra/dijkstra.cpp
@@ -4,19 +4,27 @@
 #include <ctime>
 class coordinate {
 	int x, y;
+	bool visited;
 public:
 	void setX(int x) {this->x = x;}
 	void setY(int y) {this->y = y;}
+	void setVisited(bool v) {this->visited = v;}
 	int getX() {return x;}
 	int getY() {return y;}
+	bool isVisited() {return visited;}
+	coordinate(int x, int y) {
+		setX(x);
+		setY(y);
+		setVisited(false);
+	}
 };
-void displayGrid(int l, int w, coordinate coords[], std::vector<coordinate> path) {
+void displayGrid(int l, int w, std::vector<coordinate> coords, std::vector<coordinate> path) {
 	for(int i=0; i<l; i++) {
 		for(int j=0; j<w; j++) {
-			if((i == coords[0].getX() && j == coords[0].getY()) || (i == coords[1].getX() && j == coords[1].getY())) {
-				std::cout << "[]\t";
+			for(int k=0; k<coords.size(); k++) {
+				if(i == coords[k].getX() && j == coords[k].getY()) {std::cout << "[]\t";}
+				else {std::cout << "*\t";}
 			}
-			else {std::cout << "*\t";}
 		}
 		std::cout << "\n";
 	}
@@ -25,12 +33,22 @@ void dijkstra() {
 	//Note: if there are multiple paths of equal length, use a PRNG to pick a path
 }
 int main() {
-	int length, width, temp; coordinate coords[2]; std::vector<coordinate> path;
-	std::cout << "\nDijkstra's Algorithm 1.0\nEnter the length of the grid:\n"; std::cin >> length;
-	std::cout << "Enter the width of the grid:\n"; std::cin >> width;
-	std::cout << "Enter the x-position of the starting point:\n"; std::cin >> temp; coords[0].setX(temp);
-	std::cout << "Enter the y-position of the starting point:\n"; std::cin >> temp; coords[0].setY(temp);
-	std::cout << "Enter the x-position of the end point:\n"; std::cin >> temp; coords[1].setX(temp);
-	std::cout << "Enter the y-position of the end point:\n"; std::cin >> temp; coords[1].setY(temp);
+	int length = 0, width = 0, foo, bar, temp; std::vector<coordinate> coords, path;
+	std::cout << "\nDijkstra's Algorithm 1.0\nEnter the x-position of the starting point:\n"; std::cin >> foo;
+	std::cout << "Enter the y-position of the starting point:\n"; std::cin >> bar;
+	coordinate *c = new coordinate(foo, bar); coords.push_back(*c); delete c;
+	std::cout << "Enter the x-position of the end point:\n"; std::cin >> foo;
+	std::cout << "Enter the y-position of the end point:\n"; std::cin >> bar;
+	c = new coordinate(foo, bar); coords.push_back(*c); delete c;
+	std::cout << "How many other points would you like? Please enter here:\n"; std::cin >> temp;;
+	for(int i=0; i<temp; i++) {
+		std::cout << "Enter the x-position of a point:\n"; std::cin >> foo;
+		std::cout << "Enter the y-position of a point:\n"; std::cin >> bar;
+		c = new coordinate(foo, bar); coords.push_back(*c); delete c;
+	}
+	for(int i=0; i<coords.size(); i++) {
+		if(coords[i].getX() > length) {length = coords[i].getX();}
+		if(coords[i].getY() > width) {width = coords[i].getY();}
+	}
 	displayGrid(length, width, coords, path); //tests grid for now
 }
