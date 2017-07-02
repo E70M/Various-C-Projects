0b8573161f44d18affbdcb9d061af8f9df5df6db
E70M <eitanmyron@outlook.com>
2017-07-02 13:15:32 -0700

    Added test files

--- ChatScanner/chatscanner.cpp
+++ ChatScanner/chatscanner.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <fstream>
+int lineCount(std::ifstream file) {
+	int foo;
+	return foo;
+}
 int main() {
 	std::ifstream chatlog("chatlog.txt"), blacklist("blacklist.txt"); 
+	int cl_length = lineCount(chatlog), bl_length = lineCount(blacklist);
 }
