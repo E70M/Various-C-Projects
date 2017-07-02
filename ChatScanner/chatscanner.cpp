510b30ce6a8ba110c8749e115c4690982ff2c658
E70M <eitanmyron@outlook.com>
2017-07-02 14:14:30 -0700

    Test print statements are working

--- ChatScanner/chatscanner.cpp
+++ ChatScanner/chatscanner.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <fstream>
-int lineCount(std::ifstream file) {
-	int foo;
-	return foo;
+int lineCount(std::string filename) {
+	std::ifstream file(filename.c_str()); file.unsetf(std::ios_base::skipws);
+    unsigned line_count = std::count(std::istream_iterator<char>(file), std::istream_iterator<char>(), '\n');
+    return line_count;
+}
+std::string get_bl_keyword(int pos, std::string filename) {
+	std::ifstream file(filename.c_str()); std::string line, bar;
+	for(int i=0; i<pos; i++) {
+		std::getline(file, line);
+	}
+	bar = line;
+	return bar;
 }
 int main() {
 	std::ifstream chatlog("chatlog.txt"), blacklist("blacklist.txt"); 
-	int cl_length = lineCount(chatlog), bl_length = lineCount(blacklist);
+	int cl_length = lineCount("chatlog.txt"), bl_length = lineCount("blacklist.txt");
+	std::cout << "The chat log is " << cl_length << " lines long.\n"; //test length
+	std::cout << "The blacklist is " << bl_length << " lines long.\n"; //test length
+	std::cout << "Test keyword: " << get_bl_keyword(1, "blacklist.txt") << "\n"; //test keyword selection
 }
