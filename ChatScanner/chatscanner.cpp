92d9defee20253d1e65a8f19dc52169e3f3e85db
E70M <eitanmyron@outlook.com>
2017-07-23 22:51:52 -0700

    Rewrote algorithm
    Turns out that the algorithm method I wrote to scan through the files
    already exists as an object method of std::string.

--- ChatScanner/chatscanner.cpp
+++ ChatScanner/chatscanner.cpp
@@ -6,18 +6,13 @@ std::string get_bl_keyword(int pos, std::string filename) {
 	return line;
 }
 void checkChat(std::string line, int blPos) {
-	for(int k=0; k<line.length(); k++) {
-		for(int j=line.length(); j>k; j--) {
-			if(line.substr(j, k) == get_bl_keyword(blPos, "blacklist.txt")) {
-				std::cout << "Flagged user: " << line.substr(0, line.find(":")) << ". Want to ban them?\n";
-			}
-		}
-	}
+	int pos = line.find(get_bl_keyword(blPos, "blacklist.txt"));
+	if(pos >= 0) {std::cout << "Flagged user: " << line.substr(0, line.find(":")) << ". Want to ban them?\n";}
 }
 int main() {
 	std::ifstream chatlog("chatlog.txt"), blacklist("blacklist.txt"); std::string line; blacklist.unsetf(std::ios_base::skipws);
 	int bl_length = std::count(std::istream_iterator<char>(blacklist), std::istream_iterator<char>(), '\n') + 1;
-	for(int i=1; i<bl_length+1; i++) {
+	for(int i=1; i<=bl_length; i++) {
 		std::cout << "Scanning chat for: " << get_bl_keyword(i, "blacklist.txt") << "\n";
 		while(!chatlog.eof()) {std::getline(chatlog, line); checkChat(line, i);}
 		std::getline(chatlog, line); checkChat(line, bl_length);
