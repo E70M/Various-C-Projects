f8886dd5b91f4d597aa2d3766eeb86e099f5caeb
E70M <eitanmyron@outlook.com>
2018-11-25 12:10:21 -0800

    Dijkstra part 1
    Revamped project, implemented logic for building adjancency matrix

--- Dijkstra/dijkstra.cpp
+++ Dijkstra/dijkstra.cpp
@@ -2,53 +2,37 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-class coordinate {
-	int x, y;
-	bool visited;
+class node {
+	double weight;
 public:
-	void setX(int x) {this->x = x;}
-	void setY(int y) {this->y = y;}
-	void setVisited(bool v) {this->visited = v;}
-	int getX() {return x;}
-	int getY() {return y;}
-	bool isVisited() {return visited;}
-	coordinate(int x, int y) {
-		setX(x);
-		setY(y);
-		setVisited(false);
-	}
+	void setWeight(double weight) { this->weight = weight; }
 };
-void displayGrid(int l, int w, std::vector<coordinate> coords, std::vector<coordinate> path) {
-	for(int i=0; i<l; i++) {
-		for(int j=0; j<w; j++) {
-			for(int k=0; k<coords.size(); k++) {
-				if(i == coords[k].getX() && j == coords[k].getY()) {std::cout << "[]\t";}
-				else {std::cout << "*\t";}
+int main() {
+	int nodeCount;
+	std::cout << "Enter number of nodes:" << std::endl;
+	std::cin >> nodeCount;
+	double weights[nodeCount][nodeCount];
+	for(int i = 0; i < nodeCount; i++) {
+		for(int j = 0; j < nodeCount; j++) {
+			if(i == j) {
+				weights[i][j] = 0;
+			} else if(j < i) {
+				weights[i][j] = weights[j][i];
+			} else {
+				std::cout << "Enter weight of path from node " << i + 1 << " to node " << j + 1 << ":\n";
+				std::cin >> weights[i][j];
+				if(weights[i][j] < 0) {
+					weights[i][j] *= -1;
+				}
 			}
 		}
-		std::cout << "\n";
 	}
-}
-void dijkstra() {
-	//Note: if there are multiple paths of equal length, use a PRNG to pick a path
-}
-int main() {
-	int length = 0, width = 0, foo, bar, temp; std::vector<coordinate> coords, path;
-	std::cout << "\nDijkstra's Algorithm 1.0\nEnter the x-position of the starting point:\n"; std::cin >> foo;
-	std::cout << "Enter the y-position of the starting point:\n"; std::cin >> bar;
-	coordinate *c = new coordinate(foo, bar); coords.push_back(*c); delete c;
-	std::cout << "Enter the x-position of the end point:\n"; std::cin >> foo;
-	std::cout << "Enter the y-position of the end point:\n"; std::cin >> bar;
-	c = new coordinate(foo, bar); coords.push_back(*c); delete c;
-	std::cout << "How many other points would you like? Please enter here:\n"; std::cin >> temp;;
-	for(int i=0; i<temp; i++) {
-		std::cout << "Enter the x-position of a point:\n"; std::cin >> foo;
-		std::cout << "Enter the y-position of a point:\n"; std::cin >> bar;
-		c = new coordinate(foo, bar); coords.push_back(*c); delete c;
+	/*
+	for(int i = 0; i < nodeCount; i++) {
+		for(int j = 0; j < nodeCount; j++) {
+			std::cout << weights[i][j] << " ";
+		}
+		std::cout << std::endl;
 	}
-	for(int i=0; i<coords.size(); i++) {
-		if(coords[i].getX() > length) {length = coords[i].getX();}
-		if(coords[i].getY() > width) {width = coords[i].getY();}
-	}
-	displayGrid(length, width, coords, path); //tests grid for now
+	*/ //for test purposes only, displays adjacency matrix
 }
